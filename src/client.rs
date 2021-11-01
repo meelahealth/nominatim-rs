b691c3263c5320cff72d9b967a57b88851b770b5
John Toohey <john_t@mailo.com>
2021-11-01 11:30:57 +0000

    Implemented wasm32 support.

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,7 @@
+#[cfg(target_arch="wasm32")]
+use reqwest::header::HeaderMap;
+use thiserror::Error;
+
 /// A nominatim client that is binded to the nominatim web api.
 pub struct Client {
     /// The user agent of your service. This is required by the Nominatim
@@ -11,6 +15,17 @@ pub struct Client {
     /// The base URL
     pub base_url: reqwest::Url,
     pub client: reqwest::Client,
+}
+
+/// An error that may be returned when creating a new
+/// client.
+#[derive(Error, Debug)]
+pub enum NewError {
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[cfg(target_arch = "wasm32")]
+    #[error("user agent error: {0}")]
+    UserAgent(#[from] reqwest::header::InvalidHeaderValue),
 }
 
 impl Client {
@@ -29,10 +44,22 @@ impl Client {
         base_url: reqwest::Url,
         user_agent: String,
         email: Option<String>,
-    ) -> reqwest::Result<Self> {
+    ) -> Result<Self, NewError> {
         Ok(Self {
+            // Normally we can just use the
+            // `ClientBuilder.user_agent` meth od but it doesn't work with
+            // wasm. No idea why.
+            #[cfg(not(target_arch = "wasm32"))]
             client: reqwest::Client::builder()
                 .user_agent(&user_agent)
+                .build()?,
+            #[cfg(target_arch = "wasm32")]
+            client: reqwest::Client::builder()
+                .default_headers({
+                    let mut headers = HeaderMap::new();
+                    headers.insert(reqwest::header::USER_AGENT, user_agent.parse()?);
+                    headers
+                })
                 .build()?,
             base_url,
             user_agent,
